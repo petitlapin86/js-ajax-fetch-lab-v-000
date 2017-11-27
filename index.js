6e46fe938b0d5801c752e341bca2af94033012d7
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-27 18:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const userName = ''
 const baseApi = 'https://api.github.com/'
 const fork = `${userName}/javascript-fetch-lab`
 
+//Issue and Repo objects and templates
 
 function Issue(attributes){
   this.title = attributes.title;
@@ -23,23 +24,7 @@ Repo.prototype.template = function(){
   return template;
 };
 
-
-
- getIssues(data) {
-  fetch(`${baseApi}repos/${fork}/issues`).
-    then(resp => {
-      resp.json().then( data => {
-        for (let i = 0; i < data.length; i++){
-          displayIssue(new Issue(data[i]));
-        }
-      } )
-    })
-}
-
-function showIssues(json) {
-  $('#issues').append(issue.template())
-
-}
+//Create an issue through the Github API
 
 function createIssue() {
   const issueTitle = document.getElementById('title').value
@@ -54,25 +39,43 @@ function createIssue() {
   }).then(resp => getIssues())
 }
 
+//Fetch all issues through the Github API and display / append to the DOM
 
+function getIssues(data) {
+  fetch(`${baseApi}repos/${fork}/issues`).
+    then(resp => {
+      resp.json().then( data => {
+        for (let i = 0; i < data.length; i++){
+          displayIssue(new Issue(data[i]));
+        }
+      } )
+    })
+}
 
-function showResults(json) {
+function displayIssue(issue) {
   $('#issues').append(issue.template())
 }
 
+//Fetch and show repo info
+
+
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  fetch(`${baseApi}repos/${repo}/forks`, {
-   method: 'post',
-   headers: {
-     'Authorization': `token ${getToken()}`
-   }
- }).then(resp => {
-   let repo = new Repo(resp);
-   showForkedRepo(repo);
- })
-}//use fetch to fork it!
 
+  fetch(`${baseApi}repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    }
+  }).then(resp => {
+    let repo = new Repo(resp);
+    showForkedRepo(repo);
+  })
+}
+
+function showForkedRepo(repo) {
+  $('#results').append(repo.template())
+}
 
 
 function getToken() {
