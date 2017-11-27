7e786560dd703a592c8749d3b852426db30e7c27
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-27 17:57:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,16 +51,7 @@ function forkRepo() {
 }//use fetch to fork it!
 
 
+
 function getToken() {
-  const token = '39380f11db515d01709a391b20d05865f160cf64';
-
-  fetch('https://api.github.com/user/repos', {
-    headers: {
-      Authorization: `token ${token}`
-    }
-  }).then(res => res.json()).then(json => console.log(json));
-
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
